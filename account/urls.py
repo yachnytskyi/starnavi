049f16b5582c57ee7de3f85369311c3935c96f54
yachntskyi <yachnytskyi1992@gmail.com>
2020-06-29 23:18:55 +0300

    Little fix for account urls

--- account/urls.py
+++ account/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('check_if_account_exists/', does_account_exist_view, name="check_if_account_exists"),
     path('change_password/', ChangePasswordView.as_view(), name="change_password"),
     path('properties/', account_properties_view, name="properties"),
-    path('properties/update/', update_account_view, name="update"),
+    path('properties/update', update_account_view, name="update"),
     path('login/', ObtainAuthTokenView.as_view(), name="login"),
     path('register/', registration_view, name="register"),
     path('logout/', Logout.as_view()),
